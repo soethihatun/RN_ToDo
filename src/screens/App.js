2be210b44f862e6b1ea798e20a9e7f57c66e8d4f
Soe Thiha Tun <soethihatun@supers.team>
2019-12-11 17:47:27 +0630

    initial commint

---
+++ src/screens/App.js
@@ -0,0 +1,32 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+class App extends React.Component {
+  render() {
+    return (
+    <View>
+      <Text>App</Text>
+    </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
